fd7da32d85ff2ca78215c1f1f834c46c3cd4e763
unknown <viniciusbeloti7@gmail.com>
2020-09-11 16:44:07 -0300

    feat: add Chat

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Chat from "./components/Chat";
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
 
           <Switch>
             <Route path="/room/:roomId">
-              <h1>Chat</h1>
+              <Chat />
             </Route>
 
             <Route path="/">
---
+++ src/components/Chat/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+const Chat = () => {
+  return (
+      <div className="chat">
+          
+      </div>
+  );
+}
+
+export default Chat;
