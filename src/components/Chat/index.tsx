3698270a9ebc99751260cdb731d3ed9d30602340
unknown <viniciusbeloti7@gmail.com>
2020-09-11 16:47:53 -0300

    chore: add useParams to Chat

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 import './styles.css';
 
+interface IUseParams {
+    roomId: string;
+}
+
 const Chat = () => {
+    const { roomId } = useParams<IUseParams>();
+
   return (
       <div className="chat">
-          
+          {roomId}
       </div>
   );
 }
