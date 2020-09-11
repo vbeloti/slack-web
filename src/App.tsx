85b7708f8d80f84595466be6b963dbb56aa6f190
unknown <viniciusbeloti7@gmail.com>
2020-09-11 15:25:20 -0300

    feat: add ímport Header to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello World</h1>
+      <Header />
     </div>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
+import React from "react";
+import Avatar from "@material-ui/core/Avatar/Avatar";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
 
-import './styles.css';
+import "./styles.css";
 
 const Header: React.FC = () => {
   return (
-      <div className="header">
-          <div className="header__left">
-
-          </div>
-          <div className="header__search">
-
-          </div>
-          <div className="header__right">
-
-          </div>
+    <div className="header">
+      <div className="header__left">
+        <Avatar className="header__avatar" src="Avatar" />
+        <AccessTimeIcon />
       </div>
+      <div className="header__search"></div>
+      <div className="header__right"></div>
+    </div>
   );
-}
+};
 
 export default Header;
