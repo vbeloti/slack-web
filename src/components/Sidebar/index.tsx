320ac81fa6f3955a88d2af82ae1456df72349985
unknown <viniciusbeloti7@gmail.com>
2020-09-11 16:05:27 -0300

    chore: add all icons to Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import InboxIcon from "@material-ui/icons/Inbox";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+import DraftsIcon from "@material-ui/icons/Drafts";
 
 import "./styles.css";
+import SidebarOption from "../SidebarOption";
 
 const Sidebar = () => {
   return (
@@ -17,6 +28,18 @@ const Sidebar = () => {
         </div>
         <CreateIcon />
       </div>
+      <SidebarOption Icon={InsertCommentIcon} title="Tópicos" />
+      <SidebarOption Icon={InboxIcon} title="Menções & reações" />
+      <SidebarOption Icon={DraftsIcon} title="items salvos" />
+      <SidebarOption Icon={BookmarkBorderIcon} title="Procurar canal" />
+      <SidebarOption Icon={PeopleAltIcon} title="Pessoas & usuários" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={FileCopyIcon} title="Procurar arquivo" />
+      <SidebarOption Icon={ExpandLessIcon} title="Mostrar menos" />
+      <hr />
+      <SidebarOption Icon={ExpandMoreIcon} title="Canais" />
+      <hr />
+      <SidebarOption Icon={AddIcon} title="Add Canal" />
     </div>
   );
 };
--- src/components/SidebarOption/index.tsx
+++ src/components/SidebarOption/index.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import { SvgIconProps } from "@material-ui/core";
+import React, { ComponentType } from "react";
 
-import './styles.css';
+import "./styles.css";
 
-const SidebarOption = () => {
-  return (
-      <div className="src">
-          
-      </div>
-  );
+interface ISidebarOption {
+  Icon?: ComponentType<SvgIconProps>;
+  title: string;
 }
+
+const SidebarOption = ({ Icon, title }: ISidebarOption) => {
+  return (
+    <div className="sidebarOption">
+      {Icon && <Icon className="sidebarOption__icon" />}
+      {Icon ? (
+        <h3>{title}</h3>
+      ) : (
+        <h3 className="sidebarOption__channel">
+          <span className="sidebarOption__hash">#</span>
+          {title}
+        </h3>
+      )}
+    </div>
+  );
+};
 
 export default SidebarOption;
