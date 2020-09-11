42a4f27b9aca5a8a5ecbe8022d2744ba3809009e
unknown <viniciusbeloti7@gmail.com>
2020-09-11 16:15:59 -0300

    feat: add firebase config connection

---
+++ src/config/connectionFirebase.example.ts
@@ -0,0 +1,27 @@
+import * as firebase from 'firebase/app';
+
+import 'firebase/firestore';
+import 'firebase/database';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "apiKey",
+    authDomain: "authDomain",
+    databaseURL: "databaseURL",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId",
+    measurementId: "measurementId"
+};
+
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider};
+
+export default db;
