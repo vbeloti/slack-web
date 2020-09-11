28445bd9d8c428db5f17566b4e6562949d3185df
unknown <viniciusbeloti7@gmail.com>
2020-09-11 15:20:32 -0300

    feat: add Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+const Header: React.FC = () => {
+  return (
+      <div className="header">
+          
+      </div>
+  );
+}
+
+export default Header;
