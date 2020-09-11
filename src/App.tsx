eac3091e96c4ce9c3f31c2377f377567b6ff5121
unknown <viniciusbeloti7@gmail.com>
2020-09-11 16:42:42 -0300

    feat: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Header />
+      <Router>
+        <Header />
 
-      <div className="app__body">
-        <Sidebar />
-      </div>
+        <div className="app__body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/room/:roomId">
+              <h1>Chat</h1>
+            </Route>
+
+            <Route path="/">
+              <h1>HOme</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
