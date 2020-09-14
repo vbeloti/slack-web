3da8270343dc5b1aaf0437d05e3fe62e2caa04db
unknown <viniciusbeloti7@gmail.com>
2020-09-14 11:54:15 -0300

    chore: add verification user login

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
@@ -6,24 +6,31 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Chat from "./components/Chat";
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <div className="app">
       <Router>
-        <Header />
+        {!user ? (
+          <h1>Login</h1>
+        ) : (
+          <>
+            <Header />
+            <div className="app__body">
+              <Sidebar />
 
-        <div className="app__body">
-          <Sidebar />
+              <Switch>
+                <Route path="/room/:roomId">
+                  <Chat />
+                </Route>
 
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-
-            <Route path="/">
-              <h1>HOme</h1>
-            </Route>
-          </Switch>
-        </div>
+                <Route path="/">
+                  <h1>HOme</h1>
+                </Route>
+              </Switch>
+            </div>
+          </>
+        )}
       </Router>
     </div>
   );
